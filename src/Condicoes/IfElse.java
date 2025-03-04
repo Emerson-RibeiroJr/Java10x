cdcf4fb47182c8ebfadd55458889449baacf1e65
Emerson-RibeiroJr <emerson.ribeiro@hotmail.com>
2025-03-04 19:40:51 -0300

    IfElse

--- src/Condicoes/IfElse.java
+++ src/Condicoes/IfElse.java
@@ -9,19 +9,42 @@ public class IfElse {
         * Objetivo: Passar o ninja de nivel de acordo com o número de missões
         * */
 
-        //Ninja Kakashi Hatake
-        String name = "Kakashi Hatake";
-        String rank;
-        int idade = 31;
-        boolean hokage = false;
-        short numMissoes = 1141;
+        //Ninja Rock Lee
+        String nameL = "Rock Lee";
+        String rankL;
+        int idadeL = 15;
+        boolean hokageL = false;
+        short numMissoesL = 37;
 
-        //se (condicao) {faca isso}
+        // se (condicao) {faca isso}
         // "&&" = E também
-        if (numMissoes >= 3500 && idade >= 35) {
+        if (numMissoesL == 20 && idadeL > 13) {
+            System.out.println("Promovido para Rank: Chunnin");
+        }
+        // se if = false (condicao) {faca isso}
+        else if (numMissoesL >= 35 && idadeL >= 15) {
+            System.out.println("Promovido para Rank: Jounin!");
+        }
+        // se não (condicao) {faca isso}
+        else {
+            System.out.println("Ainda se mantém no Rank: Gennim");
+        }
+
+        System.out.println(); //Pulando linha --> Mudar isso caso esta não for a maneira correta de se fazer.
+
+        //Ninja Kakashi Hatake
+        String nameK = "Kakashi Hatake";
+        String rankK;
+        int idadeK = 31;
+        boolean hokageK = false;
+        short numMissoesK = 1141;
+
+        //if (condicao) {faca isso}
+        // "&&" = E também
+        if (numMissoesK >= 1000 && idadeK >= 30) {
             System.out.println("Kakashi está pronto para concorrer à Hokage.");
         }
-        //se não (condicao) {faca isso}
+        //else (condicao) {faca isso}
         else {
             System.out.println("Kakashi ainda não está pronto para essa promoção.");
         }
