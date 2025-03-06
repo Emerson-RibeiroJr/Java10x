904efbc184d1af566856f84676a926089287a52e
Emerson-RibeiroJr <emerson.ribeiro@hotmail.com>
2025-03-06 08:31:45 -0300

    IniciandoDesafio1

---
+++ src/Desafios/Desafio1.java
@@ -0,0 +1,35 @@
+package Desafios;
+
+public class Desafio1 {
+    public static void main(String[] args) {
+        /*
+        * Criar 3 ninjas da Vila da Folha (Konoha) e suas missoes
+        * Ninja deve ter: nome (String), idade (int), missoesConcluidas (int)
+        * Em missao: nomeMissao (String), nivelMissao (char), statusMissao (boolean)
+        * Níveis das missões (D, C, B, A, S)
+        * */
+        /*
+        * Mais dificuldade, aprimorando conhecimentos.
+        * Usar condicionais para verificar se o ninja pode concluir a missão baseado na idade
+        * If; Else If; Else; &&;
+        * < 15 anos = D, C
+        * >= 15 = todas missoes
+        * */
+
+        //Ninja 1 - Kakashi Hatake - 31 anos
+        String nameK = "Kakashi";
+        int idadeK = 31;
+
+
+
+        //Ninja 2 - Konohamaru Sarutobi - 12 anos
+
+
+
+        //Ninja 3 - Shikamaru Nara - 17 anos
+
+
+
+
+    }
+}
