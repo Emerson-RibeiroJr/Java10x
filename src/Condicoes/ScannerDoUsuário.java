134950588d4f2885916cb45a7010576b65fc39f3
Emerson-RibeiroJr <emerson.r.jr@hotmail.com>
2025-03-17 14:13:00 -0300

    Aplicacao_de_Scanner

--- src/Condicoes/ScannerDoUsuário.java
+++ src/Condicoes/ScannerDoUsuário.java
@@ -1,20 +1,36 @@
 package Condicoes;
+import java.util.Scanner;
 
 public class ScannerDoUsuário {
-    // Exame Chunin: Verificando se o ninja tem idade suficiente para competir
-import java.util.Scanner;
-    public class ExameChunin {
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Digite a sua idade, jovem ninja: ");
-            int idade = scanner.nextInt();
-            if (idade >= 12) {
-                System.out.println("Parabéns! Você pode participar do Exame Chunin!");
-            } else {
-                System.out.println("Você ainda é muito jovem para o Exame Chunin. Continue treinando!");
-            }
-            scanner.close();
-        }
-    }
+    public static void main(String[] args) {
+        /*
+         * Scanner = É um modo de trazer o usuário para dentro da aplicação.
+         * Objetivo = O usuário vai criar um campeão. Nós validaremos os dados.
+         *
+         * */
+        Scanner caixaDeTexto = new Scanner(System.in);
 
+        // Título
+        System.out.println("----------------------------------");
+        System.out.println("Criando um campeão");
+        System.out.println("----------------------------------");
+        // Recebe Nome
+        System.out.println("Digite o nome");
+        String nomeCampeao = caixaDeTexto.nextLine();
+        System.out.println("Nome: " + nomeCampeao);
+        // Recebe idade
+        System.out.println("Digite a idade");
+        int idadeCampeao = caixaDeTexto.nextInt();
+        System.out.println("Idade: " + idadeCampeao + " anos");
+
+        // Tratamento de dados
+        if (idadeCampeao >= 18 ){
+            System.out.println("Campeão maior de idade, já pode ir para o Monte Targon");
+        } else {
+            System.out.println("Campeão ainda é muito novo para se arriscar no Monte Targon");
+        }
+
+        // Sempre fechar o Scanner
+        caixaDeTexto.close();
+    }
 }
