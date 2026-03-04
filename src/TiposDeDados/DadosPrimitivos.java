4f632615e272a2b53b836a097f1307e845c9e02c
Emerson Pessoa <emerson.r.jr@hotmail.com>
2026-03-04 19:57:07 -0300

    second_Day

--- src/TiposDeDados/DadosPrimitivos.java
+++ src/TiposDeDados/DadosPrimitivos.java
@@ -7,17 +7,19 @@ public class DadosPrimitivos {
         * Objetivo da aula: Criar um Ninja. - Kakashi -
         */
 
-        int idade = 31; //Tem um valor máximo que pode ser colocado (2 147 483 647)//
+        int idade = 31; //Tem um valor máximo que pode ser colocado (2 147 483 647)
         double altura = 1.81;
-        char inicial = 'K';
+        char inicial = 'K'; // Sempre colocar o valor com 'aspas simples'
         boolean vivoOuMorto = true;
-        Long saldoBancario = 9958215L; //Ele serve para extender o valor máximo do int (9 223 372 036 854 775 807)//
+        int saldoBancarioAnterior = 399833; // Valor maximo int: 2 147 483 647
+        Long saldoBancario = 9958218L; //Ele serve para extender o valor máximo do int (9 223 372 036 854 775 807)
 
         System.out.println(idade); //Comando para mostrar para o usuário
         System.out.println(saldoBancario);
         System.out.println(altura);
         System.out.println("Saldo Bancario é: " + saldoBancario);
         System.out.println("Ele está: " + vivoOuMorto);
+
 
 
     }
